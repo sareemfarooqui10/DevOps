18de1bbb6c7fa49be0bb88b5664511f0da2a69b8
sareemfarooqui10 <sareemaishasaif@gmail.com>
2025-05-05 17:23:15 +0300

    Check

--- src/data/hero.ts
+++ src/data/hero.ts
@@ -1,5 +1,5 @@
 export const heroDetails = {
-    heading: 'Smart, Secure, Simple Financial Management',
+    heading: 'This presents DevOps project 21K',
     subheading: 'From effortless budgeting to real-time investment insights, Finwise puts you in control of your money like never before',
     centerImageSrc: '/images/hero-mockup.webp',
 }
