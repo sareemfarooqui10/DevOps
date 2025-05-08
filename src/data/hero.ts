1a5cae86fce0706f51a4e6c7344e67f48d39c913
sareemfarooqui10 <sareemaishasaif@gmail.com>
2025-05-08 17:45:24 +0300

    Test Run

--- src/data/hero.ts
+++ src/data/hero.ts
@@ -1,5 +1,5 @@
 export const heroDetails = {
-    heading: 'This presents DevOps project 21K',
+    heading: 'DevOps project presented by DevOps Batch 21K',
     subheading: 'From effortless budgeting to real-time investment insights, Finwise puts you in control of your money like never before',
     centerImageSrc: '/images/hero-mockup.webp',
 }
