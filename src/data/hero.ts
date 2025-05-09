a4974b4dcfc3e80c1a0e2fa195c3ad75d8e2ce07
Hussain-malik03 <mhussain.malik89@gmail.com>
2025-05-10 00:40:35 +0500

    first-commit

--- src/data/hero.ts
+++ src/data/hero.ts
@@ -1,5 +1,5 @@
 export const heroDetails = {
-    heading: 'DevOps project presented by DevOps Batch 21K',
+    heading: 'procom project presented by DevOps Batch 21K',
     subheading: 'From effortless budgeting to real-time investment insights, Finwise puts you in control of your money like never before',
     centerImageSrc: '/images/hero-mockup.webp',
 }
